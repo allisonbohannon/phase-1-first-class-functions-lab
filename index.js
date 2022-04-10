94cd6f7d7fcdec626076e68f027627271dc5329c
Allison Bohannon <akboh.github@gmail.com>
2022-04-10 13:45:37 -0700

    commit tree f54663eb411a40318b938c52d03a88b94c396fc6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+   return array.slice(0,2)
+}; 
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(-2); 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+function createFareMultiplier(int) {
+    return function(fare) { return fare * int}; 
+}
+
+const fareDoubler = fare => {
+    const doubledFare = createFareMultiplier(2); 
+    return doubledFare(fare);
+};
+
+const fareTripler = fare => {
+    const tripledFare = createFareMultiplier(3); 
+    return tripledFare(fare); 
+}
+
+function selectDifferentDrivers(array, funct) {
+    return funct(array); 
+}
